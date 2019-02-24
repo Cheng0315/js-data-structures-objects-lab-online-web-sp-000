216b12ea179de59225df0cf43fca4f65240a2824
Cheng Vue <csalvue@gmail.com>
2019-02-24 10:09:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
-  delete Object.assign({}, driver)[key]
+  const newObj =  Object.assign({}, driver);
+  delete newObj[key];
+  return newObj;
 }
